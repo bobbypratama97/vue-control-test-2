45b6544c07cd822d849b6a3d1d259ffa51ce4d20
Bobby Pratama <bpratama77@gmail.com>
2020-03-21 11:27:29 +0700

    vuetify setup and login form setup

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,72 +2,18 @@
 <template>
   <v-app id="inspire">
     <v-content>
-      <v-container
-        class="fill-height"
-        fluid
-      >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col
-            cols="12"
-            sm="8"
-            md="4"
-          >
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
             <v-card class="elevation-12">
-              <v-toolbar
-                color="primary"
-                dark
-                flat
-              >
-                <v-toolbar-title>Login form</v-toolbar-title>
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Bobby's Cafe</v-toolbar-title>
                 <v-spacer />
-                <v-tooltip bottom>
-                  <template v-slot:activator="{ on }">
-                    <v-btn
-                      :href="source"
-                      icon
-                      large
-                      target="_blank"
-                      v-on="on"
-                    >
-                      <v-icon>mdi-code-tags</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
-                  <template v-slot:activator="{ on }">
-                    <v-btn
-                      icon
-                      large
-                      href="https://codepen.io/johnjleider/pen/pMvGQO"
-                      target="_blank"
-                      v-on="on"
-                    >
-                      <v-icon>mdi-codepen</v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Codepen</span>
-                </v-tooltip>
               </v-toolbar>
               <v-card-text>
                 <v-form>
-                  <v-text-field
-                    label="Login"
-                    name="login"
-                    prepend-icon="person"
-                    type="text"
-                  />
-
-                  <v-text-field
-                    id="password"
-                    label="Password"
-                    name="password"
-                    prepend-icon="lock"
-                    type="password"
-                  />
+                  <v-text-field label="Login" name="login" type="text" />
+                  <v-text-field id="password" label="Password" name="password" type="password" />
                 </v-form>
               </v-card-text>
               <v-card-actions>
@@ -83,9 +29,7 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      source: String,
-    },
-  }
+export default {
+  props: {}
+};
 </script>
